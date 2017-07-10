37163de9a1258814396a61cbf45bc3f776e55d5c
sepcon <nguyenvancon.91@gmail.com>
2017-07-10 18:14:29 +0700

    update script file

--- qmake_from_make.py
+++ qmake_from_make.py
@@ -1,41 +1,118 @@
 import re
 from sets import Set
-makefile = open("/home/sepcon/Documents/git/SmCgAddin/sds_adapter-rnaivi_out.gnumake", 'r')
-
-INCLUDEPATH = "INCLUDE_PATH += \\"
-SOURCE = "SOURCE += \\"
-
-setOfInclude = None
-setOfCppFiles = Set()
+import sys
+import glob
 
 
-line = makefile.readline()
-while line != '':
-    cppFile = re.search(r'\/[^%]+.cpp[\s]+\\', line)
-    if cppFile :
-        setOfCppFiles.add(cppFile.group() + "\n")
-    line = makefile.readline()
 
-makefile.seek(0)
-line = makefile.readline()
-while line != '':
+MAKEFILE_PATH_KEY = '-m'
+QMAKE_DIR_KEY = '-qm'
+
+QMAKE_FILE_EXTENSION = ".pro"
+QMAKE_FILE_TEMPLATE = 'TEMPLATE = app\nCONFIG += console c++11\nCONFIG -= app_bundle \nCONFIG -= qt\n\n\n'
+QMAKE_INCLUDEPATH_KEYWORD = "INCLUDEPATH += \\\n"
+QMAKE_SOURCE_KEYWORD = "SOURCES += \\\n"
+HEADER = "HEADERS += \\\n"
+
+
+def printHelp():
+    print '''
+    -----------------------------------USAGE--------------------------------------------
+    SYNTAX: python qmake_from_make.py -m <path_to_makefile> [-qm <path_to_qmake_output>]
+    ------------------------------------------------------------------------------------
+    '''
+def checkArguments(argumentsMap):
+    if not argumentsMap.has_key(MAKEFILE_PATH_KEY):
+        print '\nplease specify the path of makefile'
+        return False
+
+    if not argumentsMap.has_key(QMAKE_DIR_KEY):
+        argumentsMap[QMAKE_DIR_KEY] = '.'
+
+    return True
+
+def parseArguments():
+    argumentsMap = {}
+    argc = len(sys.argv)
+    if argc > 1:
+        i = 1
+        while i < argc:
+            if sys.argv[i][0] == '-':
+                argumentsMap[sys.argv[i]] = sys.argv[i + 1]
+                i = i + 2
+            else:
+                i = i + 1
+    return argumentsMap
+
+def getFileNameFromPath(filePath):
+    indexOfSlash = filePath.rfind('/')
+    if indexOfSlash == -1:
+        return filePath
+    else:
+        return filePath[indexOfSlash : len(filePath)]
+
+def getContainingDir(filePath):
+    indexOfSlash = filePath.rfind('/')
+    if indexOfSlash == -1:
+        return ""
+    else:
+        return filePath[0 : indexOfSlash]
+
+
+
+argumentsMap = parseArguments()
+if checkArguments(argumentsMap) != True:
+    printHelp()
+    exit(-1)
+
+
+
+
+
+includePathsList = None
+cppFilesList = Set()
+headerFilesList = Set()
+
+makefile = open(argumentsMap[MAKEFILE_PATH_KEY], 'r')
+makeContent = makefile.readlines()
+
+
+for line in makeContent:
+    cppMatch = re.search(r'\/[^%]+.cpp[\s]+\\', line)
+    if cppMatch :
+        cppFilesList.add(cppMatch.group() + "\n")
+
+
+for line in makeContent:
     includeMatch = re.search(r'-I.*\/ ', line)
     if includeMatch:
-        includeContent = includeMatch.group();
-        setOfInclude = includeContent.replace("-I", " \\\n")
-        # setOfInclude = Set(includePaths)
+        includeContent = includeMatch.group()
+        includePathsList = includeContent.replace("-I", " \\\n")
+        # includePathsList = Set(includePathsList)
         break
-    else:
-        line = makefile.readline()
+
+for cppFile in cppFilesList:
+    containingDir = getContainingDir(cppFile)
+    headerFilesInSameDir = glob.glob(containingDir + "/*.h")
+    for header in headerFilesInSameDir:
+        headerFilesList.add(header + ' \\\n')
 
 makefile.close()
 
-profile = open("/home/sepcon/Documents/git/SmCgAddin/sds.pro", 'w')
+qmakeFilePath = argumentsMap[QMAKE_DIR_KEY] + '/' + getFileNameFromPath(argumentsMap[MAKEFILE_PATH_KEY]) + '.pro'
+qmakeFile = open( qmakeFilePath, 'w')
+qmakeFile.write(QMAKE_FILE_TEMPLATE);
+if includePathsList :
+    qmakeFile.write(QMAKE_INCLUDEPATH_KEYWORD )
+    qmakeFile.write(includePathsList + '\n')
+    qmakeFile.write("\n\n")
 
-if setOfInclude :
-    profile.write(INCLUDEPATH + "\n")
-    profile.write(setOfInclude + '\n')
-    profile.write("\n\n")
-profile.write(SOURCE + '\n')
-profile.writelines(setOfCppFiles)
-profile.close()
+qmakeFile.write(QMAKE_SOURCE_KEYWORD)
+qmakeFile.writelines(cppFilesList)
+
+qmakeFile.write("\n\n")
+
+qmakeFile.write(HEADER)
+qmakeFile.writelines(headerFilesList)
+
+qmakeFile.close()
